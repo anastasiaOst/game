b34dde2287af7ef651a4a689c6eced209ec564a5
Anastasia Ostapenko <ostapenko.nastya.ao@gmail.com>
2022-06-18 15:53:32 +0300

    load all files

--- main.js
+++ main.js
@@ -15,11 +15,11 @@ function jump(){
     }, 300)
 }
 
-// let isAlive = setInterval (function(){
-//     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-//     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+let isAlive = setInterval (function(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
 
-//     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >=140){
-//         alert("GAME OVER!")
-//     }
-// })
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >=140){
+        alert("GAME OVER!")
+    }
+})
